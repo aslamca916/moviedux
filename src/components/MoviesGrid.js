080a410d8715bcaa0addfbc36bb5f187feb82a69
aslam_ca <aslamca1998@gmail.com>
2024-10-18 10:10:40 +0530

    complted genre filter

--- src/components/MoviesGrid.js
+++ src/components/MoviesGrid.js
@@ -4,6 +4,20 @@ import MovieCard from "./MovieCard";
 
 export default function MoviesGrid({ movies, watchlist, toggleWatchlist }) {
   const [searchTerm, setSearchTerm] = useState("");
+  const [selectedGenre, setSelectedGenre] = useState("All Genres");
+
+  const handleGenreChange = (e) => {
+    setSelectedGenre(e.target.value);
+    console.log("Genre", selectedGenre);
+    console.log("Genre", e.target.value);
+  };
+
+  const matchesGenre = (movie, genre) => {
+    return (
+      genre === "All Genres" ||
+      movie.genre.toLowerCase() === genre.toLowerCase()
+    );
+  };
 
   const handleSearchChange = (e) => {
     setSearchTerm(e.target.value);
@@ -14,8 +28,9 @@ export default function MoviesGrid({ movies, watchlist, toggleWatchlist }) {
     return movie.title.toLowerCase().includes(searchTerm.toLowerCase());
   };
 
-  const filteredMovies = movies.filter((movie) =>
-    matchesSearchTerm(movie, searchTerm)
+  const filteredMovies = movies.filter(
+    (movie) =>
+      matchesSearchTerm(movie, searchTerm) && matchesGenre(movie, selectedGenre)
   );
 
   return (
@@ -27,13 +42,24 @@ export default function MoviesGrid({ movies, watchlist, toggleWatchlist }) {
         value={searchTerm}
         onChange={handleSearchChange}
       />
-
+      <div className="filter-bar">
+        <div className="filter-slot">
+          <label>Genre</label>
+          <select
+            className="filter-dropdown"
+            value={selectedGenre}
+            onChange={handleGenreChange}
+          >
+            <option>All Genres</option>
+            <option>Action</option>
+            <option>Drama</option>
+            <option>Fantasy</option>
+            <option>Horror</option>
+          </select>
+        </div>
+      </div>
       <div className="movies-grid">
         {filteredMovies.map((movie) => (
-          // <div key={movie.id}>
-          //     <h1>{movie.title}</h1>
-          // </div>
-          
           <MovieCard
             movie={movie}
             key={movie.id}
