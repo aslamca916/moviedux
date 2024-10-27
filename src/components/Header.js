9e96312f0c964c5fa90fca274d640f032b0128ef
aslam_ca <aslamca1998@gmail.com>
2024-10-27 11:03:01 +0530

    folder restructured

--- src/components/Header.js
+++
@@ -1,9 +0,0 @@
-import react from 'react'
-
-export default function header(){
-    return(
-        <div className='header'>
-            <img className='logo' src='logo.png' width={250} height={250} alt='logo'/>
-        </div>
-    )
-} 
